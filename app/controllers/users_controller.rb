09f78257393521b1ab894440b12728a498d70267
Luan Ta <luan@tinypulse.com>
2017-07-26 19:08:31 +0700

    Initial Google Map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
     @books = @user.books
 
     @address = Address.find_by(user_id: @user.id).nearbys(10,:unit => :km)
+
     @hash = Gmaps4rails.build_markers(@address) do |a, marker|
       marker.lat a.latitude
       marker.lng a.longitude
       marker.title a.user.email
-      marker.json user:{user_id: a.user_id, email: a.user.email }
+      marker.json user:{user_id: a.user_id, email: a.user.email, phone: a.phone}
     end
   end
 
