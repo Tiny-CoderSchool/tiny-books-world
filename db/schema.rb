c3e576a487a63ded1db536d69fee12c1bd2da0e1
Hoang Phan <hoangds121@gmail.com>
2017-07-31 01:34:16 +0700

    UI bugfix

--- db/schema.rb
+++ db/schema.rb
@@ -83,16 +83,6 @@ ActiveRecord::Schema.define(version: 20170725071142) do
     t.index ["user_id"], name: "index_shelves_on_user_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
-    t.bigint "book_id"
-    t.integer "owner_id"
-    t.integer "borrower_id"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_transactions_on_book_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
